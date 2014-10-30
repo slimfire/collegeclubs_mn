737793c882f33de5e0f4ec5b8b9bfa29a857bb50
yohaZel <personalprojectyohannes@gmail.com>
2014-10-30 18:52:32 -0500

    [FEATURE add user model]

---
+++ models/user_model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+
+mongoose.connect("mongodb://db_username:db_password@ds033699.mongolab.com:33699/heroku_app28027178");
+
+var userSchema = new Schema({
+	username : String,
+	password : String,
+	hometown : String,
+	age : Number
+});
+
+var user_model = mongoose.model('user', userSchema);
+
+module.exports = user_model;
