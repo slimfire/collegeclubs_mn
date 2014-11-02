42f90fb4ce98400d81cc8b33cda1169ea75722e7
yohaZel <personalprojectyohannes@gmail.com>
2014-11-01 23:59:40 -0500

    [FEATURE] add index, login and signup response handlers

---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+
+exports.indexResponseHandler = function (req, res){
+	res.render('index');
+}
+
+exports.loginResponseHandler = function (req, res){
+	res.render('login');
+}
+exports.signupResponseHandler = function(req, res){
+	res.render('signup');
+}
