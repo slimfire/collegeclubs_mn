da3964703649ba523f9e6033d57bc677d5e17dcc
yohaZel <personalprojectyohannes@gmail.com>
2014-11-08 11:25:50 -0600

    deploy demo on heroku

--- models/user_model.js
+++ models/user_model.js
@@ -4,7 +4,7 @@ var schema = mongoose.Schema;
 
 mongoose.connect("mongodb://db_username:db_password@ds033699.mongolab.com:33699/heroku_app28027178");
 
-var userSchema = new Schema({
+var userSchema = new schema({
 	username : String,
 	password : String,
 	hometown : String,
