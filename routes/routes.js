45019ec139a4c194806de758a4de62d753aaab06
yohaZel <personalprojectyohannes@gmail.com>
2014-11-09 22:07:04 -0600

    [FEATURE] add user profile response handler

--- routes/routes.js
+++ routes/routes.js
@@ -11,3 +11,10 @@ exports.signupResponseHandler = function(req, res){
 	res.render('signup', {title: "College Clubs MN | Sign Up"});
 }
 
+exports.userProfileResponseHandler = function(req, res){
+	res.render('user_profile', {title: "College Clubs MN | User Profile"});
+}
+
+exports.signinErrorResponseHandler = function(req, res){
+	res.send("Faild to Sign in ! :(");
+}
