7e4c45c56bf9b1e37a9a2a22924e648cdc1fbca1
yohaZel <personalprojectyohannes@gmail.com>
2014-11-13 17:27:24 -0600

    [FEATURE] add user authentication on signup route

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');;
 var serveStatic = require('serve-static');
 var errorHandler = require('errorhandler');
-var routes = require('./controllers/routes.js');
+var routes = require('./routes/routes.js');
 var user_model = require('./models/user_model.js');
-var user_authentication = ('./controllers/user_authentication.js');
 var app = express();
 
 //configuration
@@ -52,12 +51,53 @@ passport.use('signin_local_strategy', new localStrategy(
 	}
 ));
 
+//user authentication on sign up
+passport.use('signup_local_strategy',new localStrategy(
+	{passReqToCallback: true},
+	function(req, username, password, done)
+	{
+		user_model.findOne({username:username},function(err, user){
+			if(err)
+			{
+				return done(err);
+			}
+			if(user == null)
+			{
+				var new_user = new user_model({
+					username: username,
+					password: password,
+					university: req.body.university,
+					hometown: req.body.hometown
+				});
+				new_user.save(function(err){
+					if(err)
+					{
+						console.log(err);
+					}
+				});
+				user = new_user;    //Assigned the variable new_user to user to authematically serialize the new user.			
+				return(done(null, user));
+			}
+			if(user.username == username)
+				{
+					return(done(null, false));
+				}		
+		});		
+	}
+));
+
+//serialize user and export the the user information so that the router update the view
 passport.serializeUser(function(user, done){
 	done(null, user.id);
+	exports._id = user.id;
+	exports.username = user.username;
+	exports.university = user.university;
+	exports.hometown = user.hometown;
 });
 
-passport.deserializeUser(function(done,user){
-	user_model.findById(function(id, err){
+//deserialize  user by quering user's document id
+passport.deserializeUser(function(id,done){
+	user_model.findById(id, function(err, user){
 		done(err,user);
 	})
 });
@@ -66,11 +106,19 @@ passport.deserializeUser(function(done,user){
 app.get('/', routes.indexResponseHandler);
 app.get('/signin', routes.signinResponseHandler);
 app.get('/signup', routes.signupResponseHandler);
-app.get('/profile', routes.profileResponseHandler);
-app.get('/failure', routes.failureResponseHandler);
+app.get('/profile', routes.userProfileResponseHandler);
+app.get('/signin_error', routes.signinErrorResponseHandler);
+app.get('/signup_error', routes.signupErrorResponseHandler);
+
 app.post('/signin',
-	passport.authenticate('signin_local_strategy', { successRedirect:'/profile', 
-													 failureRedirect: '/failure'})
-	);
+	passport.authenticate('signin_local_strategy', {successRedirect: '/profile',
+													failureRedirect: '/signin_error'
+}));
+
+app.post('/signup',
+	passport.authenticate('signup_local_strategy',
+		{successRedirect:'/profile',
+		 failureRedirect:'/signup_error'
+		}));
 
 app.listen(3000);
