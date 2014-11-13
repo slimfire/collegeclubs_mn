495926b3640df91053f3a612cf1819caf1c34ffb
yohaZel <personalprojectyohannes@gmail.com>
2014-11-13 17:39:02 -0600

    [FEATURE] add serialized user information to profile route

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,30 @@
+var user_model = require('../models/user_model.js');
+var serialized_user = require('../app.js');
 
 exports.indexResponseHandler = function (req, res){
 	res.render('index', {title: "College Clubs MN"});
 }
 
 exports.signinResponseHandler = function (req, res){
-	res.render('signin', {title:"College Clubs MN | Sign in"});
+	res.render('signin');
 }
 
 exports.signupResponseHandler = function(req, res){
-	res.render('signup', {title: "College Clubs MN | Sign Up"});
+	res.render('signup');
 }
 
 exports.userProfileResponseHandler = function(req, res){
-	res.render('user_profile', {title: "College Clubs MN | User Profile"});
+	res.render('user_profile',{
+		username: serialized_user.username,
+		university: serialized_user.university,
+		hometown: serialized_user.hometown
+	});
 }
 
 exports.signinErrorResponseHandler = function(req, res){
-	res.send("Faild to Sign in ! :(");
+	res.render('signin',{error_message: "The entered username and password does not match. Please try again."})
+}
+
+exports.signupErrorResponseHandler = function(req, res){
+	res.render('signup',{error_message: 'The entered username already exists. Please try again.'})
 }
