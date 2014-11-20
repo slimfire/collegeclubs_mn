6536629e9849c59ada15cd88478c8b2c690de1e9
yohaZel <personalprojectyohannes@gmail.com>
2014-11-19 20:06:01 -0600

    [FEATURE] add a ReST api to create a user

--- routes/rest_api.js
+++ routes/rest_api.js
@@ -1,8 +1,8 @@
-var user_models = require('../models/user_model.js');
+var user_model = require('../models/user_model.js');
 
 //ReST API
 exports.getUsersResponseHandler= function(req, res){
-		user_models.find({},{_id:0, password:0, __v:0},
+		user_model.find({},{_id:0, password:0, __v:0},
 				function(err, user)
 				{
 					if(err)
@@ -16,3 +16,33 @@ exports.getUsersResponseHandler= function(req, res){
 				}
 			);
 	};
+
+exports.postUserResponseHandler = function(req, res){
+	user_model.findOne({username: req.body.username}, function(err, user){
+		if(err)
+		{
+			throw err;
+		}
+		else
+		{
+			if(user == null)
+			{
+				var newUser = new user_model({"username": req.body.username,
+								"password": req.body.password,
+								"university": req.body.university,
+								"hometown": req.body.hometown});
+				newUser.save(function(err){
+					if(err)
+						{
+							throw err;
+						}
+				});
+				return (res.send('New user added!'));
+			}
+			if(user.username == req.body.username)
+			{
+				return(res.send("Entered username is already taked. Please try anotherone!"));
+			}
+		}
+	});
+}
