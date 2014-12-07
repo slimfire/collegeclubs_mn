0c859735f1ff107fe1912fa48ebea43a07ca0607
yohaZel <personalprojectyohannes@gmail.com>
2014-12-06 19:51:45 -0600

    [FEATURE] add clubsResponseHandler

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-var user_model = require('../models/user_model.js');
+var models = require('../models/user_model.js');
 var app = require('../app.js');
 
 
@@ -37,4 +37,25 @@ exports.signinErrorResponseHandler = function(req, res){
 
 exports.signupErrorResponseHandler = function(req, res){
 	res.render('signup',{error_message: 'Sorry, the entered e-mail already in use.'})
+}
+
+exports.clubs = function(req, res){
+	if(app.isSignedIn == true)
+	{
+		var clubs = [];
+		models.club_model.find({}, {_id:0}, function(err, clubsInfo){
+			if(err)
+			{
+				throw err;
+			}
+			else
+			{
+				res.render('clubs', {clubs: clubsInfo, username: app.username});
+;			}
+		});
+	}
+	else
+	{
+		res.send('Sorry, you are not signed in to view available clubs!');
+	}
 }
