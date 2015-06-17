a2f46813d3d591cf8a66ffe121596af1a964b74f
slimfire <personalprojectyohannes@gmail.com>
2015-06-16 22:15:12 -0700

    [FEATURE] import other data models and connect to the db

---
+++ models/models.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose'),
+	schema = mongoose.Schema;
+
+mongoose.connect("mongodb://db_username:db_password@ds051750.mongolab.com:51750/heroku_app31755176");
+
+var clubModel = require('club.js'),
+	adminModel = require('admin.js'),
+	studentModel = require('student.js'),
+	newClubRequestModel = require('newClubRequest.js');
+
+module.exports = {
+	clubModel : clubModel,
+	adminModel : adminModel,
+	studentModel : studentModel,
+	newClubRequestModel : newClubRequestModel
+};
+
+
