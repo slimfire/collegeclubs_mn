378253cfe41decf14d0908c1a1efc40dc0395189
slimfire <personalprojectyohannes@gmail.com>
2015-06-20 19:02:47 -0700

    [FIX] embed document schema into clubSchema

--- models/club.js
+++ models/club.js
@@ -1,24 +1,30 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
+var posterName = new schema({
+			firstName : String,
+			lastName : String,			
+		});
+
+var postBody = new schema({
+			content : String,
+			comments : [String]
+		});
+
+var post = new schema({
+		id : Number,
+		posterName : [posterName],
+		datePosted : Date,
+		timePosted : Date,
+		postBody : [postBody]
+	});
+
 var clubSchema = new schema({
 	clubName: String,
 	id : Number,
 	leaders : [{firstName : String, lastName : String, email : String}],
 	universityAt: String,
-	post: {
-		id : Number,
-		posterName : {
-			firstName : String,
-			lastName : String,			
-		},
-		datePosted : new Date().toDateString(),
-		timePosted : new Date().toLocaleTimeString(),
-		post : {
-			content : String,
-			comments : [String]
-		}
-	}
+	post: [post]
 }, {collection: 'clubs'});
 
 
