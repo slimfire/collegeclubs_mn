e7b0eb276c046650fce1648b24cf9a74bc2ae289
slimfire <personalprojectyohannes@gmail.com>
2015-06-25 20:01:18 -0700

    [FEATURE] add a class that handles club addition and removal

---
+++ controllers/api/club.js
@@ -0,0 +1,81 @@
+var crud = require('../../models/crud/crud.js');
+
+var Club = function(){}
+
+Club.prototype.addClub = function(clubName, universityAt, firstName, lastName, email, callback) {
+	var response;
+	var parameters = {
+		clubName: clubName,
+		universityAt: universityAt
+	};
+	crud.read('newClubRequestModel', parameters, function(data){
+		if(!data)
+		{
+			parameters.leaders = [{firstName : firstName, lastName : lastName, email : email}];
+			crud.create('newClubRequestModel', parameters, function(data){
+				response = {
+					error : null,
+					response : {
+						clubInfo : data
+					}
+				}
+				callback(response);
+			});
+		}
+		else
+		{
+			response = {
+				error : 'club already exists',
+				response : {}
+			};
+			callback(response);
+		}
+	});
+};
+
+Club.prototype.removeClub = function(clubName, universityAt, firstName, lastName, email, callback) {
+	var response;
+	var parameters = {
+		clubName: clubName,
+		universityAt: universityAt
+	};
+	crud.read('clubModel', parameters, function(data){
+		if(data)
+		{
+			parameters.requester = {
+				firstName : firstName, 
+				lastName : lastName,
+				email : email
+			};
+			crud.create('clubRemovalRequestModel', parameters, function(data){
+				if(data)
+				{
+					response = {
+						error : null,
+						response : parameters
+					};
+					callback(response);
+				}
+				else
+				{
+					response = {
+						error : 'club does not exists',
+						response : {}
+					};
+					callback(response);
+				}
+			});
+		}
+		else
+		{
+			response = {
+				error : 'club does not exist',
+				response : {}
+			}
+			callback(response);
+		}
+	});
+};
+
+
+module.exports = new Club();
