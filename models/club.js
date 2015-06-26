3db23b8fe322a1a7f9036b0615d2897fb930dc01
slimfire <personalprojectyohannes@gmail.com>
2015-06-25 19:37:42 -0700

    [FIX] remodel club data model

--- models/club.js
+++ models/club.js
@@ -1,30 +1,10 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
-var posterName = new schema({
-			firstName : String,
-			lastName : String,			
-		});
-
-var postBody = new schema({
-			content : String,
-			comments : [String]
-		});
-
-var post = new schema({
-		id : Number,
-		posterName : [posterName],
-		datePosted : Date,
-		timePosted : Date,
-		postBody : [postBody]
-	});
-
 var clubSchema = new schema({
 	clubName: String,
-	id : Number,
 	leaders : [{firstName : String, lastName : String, email : String}],
 	universityAt: String,
-	post: [post]
 }, {collection: 'clubs'});
 
 
