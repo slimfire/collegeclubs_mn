c3054ee7eb31064703bc6fadf255fc3aaf63738f
slimfire <personalprojectyohannes@gmail.com>
2015-06-26 23:57:29 -0700

    [FEATURE] add methods to retrive account information, update account information and delete account

--- controllers/api/student.js
+++ controllers/api/student.js
@@ -2,6 +2,28 @@ var crud = require('../../models/crud/crud.js');
 
 var Student = function(){}
 
+Student.prototype.getAccountInfo = function(email, callback){
+	crud.read.readByParameter('studentModel', {email : email}, function(accountInfo){
+		accountInfo.password = accountInfo.password.length;
+		callback(accountInfo);
+	});
+}
 
+Student.prototype.updateAccountInfo = function(id, update, callback){
+	crud.update.replaceDocument('studentModel', {_id : id}, update, function(upToDateAccountInfo){
+		callback(upToDateAccountInfo);
+	});
+}
+
+Student.prototype.deleteAccount = function(email, callback){
+	crud.read.readByParameter('studentModel', {email : email}, function(accountInfo){
+		if(accountInfo)
+		{
+			crud.delete.deleteByParameter('studentModel', {email : email}, function(response){
+				callback(response);
+			});
+		}
+	});
+}
 
 module.exports = new Student();
