bc48f0529b5aa62fbafe537c7cff0658b243b801
slimfire <personalprojectyohannes@gmail.com>
2015-06-27 12:55:27 -0700

    [FEATURE] add search feature on student data

---
+++ controllers/api/search/student.js
@@ -0,0 +1,35 @@
+var crud = require('../../../models/crud/crud.js');
+
+var Student = function(){}
+
+Student.prototype.getStudentsInfo = function(callback){
+	var query = {
+		query : {},
+		options : {password : 0, __v : 0}
+	};
+	crud.read.readByParameter('studentModel', query, function(studentsInfo){
+		callback(studentsInfo);
+	});
+}
+
+Student.prototype.getStudentInfo = function(email, callback){
+	var query = {
+		query : {email : email},
+		options : {password : 0, __v : 0}
+	};
+	crud.read.readByParameter('studentModel', query, function(studentInfo){
+		callback(studentInfo);
+	});
+}
+
+Student.prototype.getStudentsInfoByUniversity = function(university, callback){
+	var query = {
+		query : {university : university},
+		options : {password : 0, __v : 0}
+	};
+	crud.read.readByParameter('studentModel', query, function(studentsInfo){
+		callback(studentsInfo);
+	});
+}
+
+module.exports = new Student();
