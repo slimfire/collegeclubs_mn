23ab12649abcdb1feec351f4977c2791979172d8
slimfire <personalprojectyohannes@gmail.com>
2015-07-07 23:22:59 -0700

    [FIX] activate html5 history to remove hashtag,#, on routes

--- client/app/signin/signin.js
+++ client/app/signin/signin.js
@@ -1,18 +1,29 @@
 angular.module('collegeClubs.signin', ['ui.router', 'collegeClubs.signin.service'])
-	.config(function($stateProvider){
-		$stateProvider.state('collegeClubs.signin', {
+	.config(function($stateProvider, $locationProvider, $urlRouterProvider){
+		$stateProvider.state('signin', {
 			url : '/signin',
 			templateUrl : '/app/signin/signin.html',
 			controller : 'signinCtrl'
-		})
+		});
+		$urlRouterProvider.otherwise('/signin');
+		$locationProvider.html5Mode(true);
 	})
-	.controller('signinCtrl', function($scope, signinFactory){
-		console.log('signinCtrl');
+	.controller('signinCtrl', function($scope, signinFactory, $state){
 		$scope.signin = function(email, password, authenticationType){
 			signinFactory.signin(email, password, authenticationType)
-				.then(function(success){
-					$scope.signinResponse = success;
+				.then(function(response){
+					if(response.data.status == 200)
+					{
+						$scope.signinResponse = response.data.message;
+						$state.go('home', response.data.data); //change to user's home state
+					}
+					else if(response.data.status == 500)
+					{
+						$scope.signinResponse = response.data.message;
+					}
+					
 				}, function(error){
+					console.log('error : ', error);
 					$scope.signinResponse = error;
 				});
 		}
