5d4b538de0489fbe4825b3f126c03949e752ace5
slimfire <personalprojectyohannes@gmail.com>
2015-07-12 14:53:59 -0700

    [TASK] wrap success and error response in an object

--- client/app/profiles/student/student.js
+++ client/app/profiles/student/student.js
@@ -6,36 +6,68 @@ angular.module('collegeClubs.profiles.student', [
 		$stateProvider.state('studentProfile', {
 			url : '/profiles/student',
 			templateUrl : '/app/profiles/student/student.html',
-			controller : 'studentCtrl'
+			controller : 'studentCtrl',
+			params : {
+				username : null,
+				firstName : null,
+				lastName : null,
+				link : null,
+				phoneNumber : null,
+				university : null,
+				currentCity : null,
+				clubsLeading : null,
+				email : null
+			}
 		});
 		$urlRouterProvider.otherwise('/');
 		$locationProvider.html5Mode(true);
 	})
 	.controller('studentCtrl', function($scope, $stateParams, studentService){
+		$scope.response = {
+			success : {},
+			inputError : {},
+			serviceError : {}
+		};
 		var errorResponse = {
 			status : 500,
 			message : 'Sorry, something went wrong. Our Engineers has been notified and the service would be available soon. Please try again!',
 			data : null
 		};
-		console.log("email ",$stateParams);
+		console.log($stateParams);
 		$scope.getStudentInfo = function(){
 			studentService.getStudentInfo($stateParams.email)
 				.then(function(success){
-					$scope.studentInfo = success.data;
+					if(success.data.status == 200)
+					{
+						$scope.response.success.getStudentInfo = success.data.data;
+					}
+					else
+					{
+						$scope.response.inputError.getStudentInfo = success.data.message;	
+					}
 				}, function(error){
-					$scope.studentInfo = errorResponse;
+					$scope.response.serviceError.getStudentInfo = error;
 				})
 		}
 
 		$scope.getSimilarClubs = function(){
-			$scope.similarClubs = [];
+			$scope.response.success.getSimilarClubs = [];
+			$scope.response.inputError.getSimilarClubs = [];
+			$scope.response.serviceError.getSimilarClubs = [];
 			for(var club = 0; club < $stateParams.clubs; club++)
 			{
 				studentService.getSimilarClubs($stateParams.email, club)
 					.then(function(success){
-						$scope.similarClubs.push(success.data);
+						if(success.data.status == 200)
+						{
+							$scope.response.success.getSimilarClubs.push(success.data.data);
+						}
+						else
+						{
+							$scope.response.inputError.getSimilarClubs.push(success.data.message);	
+						}
 					}, function(error){
-						$scope.similarClubs.push(errorResponse);
+						$scope.response.serviceError.getSimilarClubs.push(errorResponse.message);
 					});
 			}
 		}
