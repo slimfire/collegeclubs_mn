7eb60116bbed4e602572b1e311a0ad97f043c001
slimfire <personalprojectyohannes@gmail.com>
2015-07-16 22:44:20 -0700

    [FEATURE] hash mongoose id and insert it as an api key on creating account

--- controllers/api/student.js
+++ controllers/api/student.js
@@ -1,29 +1,34 @@
-var crud = require('../../models/crud/crud.js');
+var crud = require('../../models/crud/crud.js'),
+		Utils = require('../utils/utils.js');
 
 var Student = function(){}
 
 Student.prototype.createAccount = function(email, password, username, firstName, lastName, university, currentCity, callback){
-	var userInfo;
-	var query = {
-		query : {
-			username : username,
-			firstName : firstName,
-			lastName : lastName,
-			university : university,	
-			currentCity : currentCity,
-			email : email,
-			password : password
-		},
-		options : {__v : 0}
+	var userInfo, key,
+			query = {
+				query : {
+					username : username,
+					firstName : firstName,
+					lastName : lastName,
+					university : university,	
+					currentCity : currentCity,
+					email : email,
+					password : password
+			},
+			options : {__v : 0}
 	};
-
 	crud.read.readByParameter('studentModel', query, function(data){
 		if(!data)
 		{
+			query.query.key = null;
 			query.query.link = null;
 			query.query.phoneNumber = null;
 			crud.create.createByParameter('studentModel', query.query , function(account){
-				callback(account);
+				key = Utils.token.hash(JSON.stringify(account._id));
+				crud.update.replaceDocument('studentModel', {email : account.email}, {key : key}, function(account){
+					account.password = null;
+					callback(account);
+				})
 			});	
 		}
 		else
