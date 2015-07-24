f60fd3cd658e115b46942afc4f2339cfd741a91c
slimfire <personalprojectyohannes@gmail.com>
2015-07-23 23:58:04 -0700

    [FEATURE] format request parameters to api endpoints

--- client/app/profiles/club/club.service.js
+++ client/app/profiles/club/club.service.js
@@ -1,50 +1,94 @@
-angular.module('collegeClubs.profiles.club.service',[])
-	.service('clubService', function($http){
+angular.module('collegeClubs.profiles.club.service',[
+		'collegeClubs.util.hashes.factory'
+	])
+	.service('clubService', function($http, hashesFactory){
 		var News = function(){}
 		var Comment = function(){}
 
-		News.prototype.listNews = function(clubName, universityAt){
+		News.prototype.listNews = function(clubName, universityAt, userType, email, key){
+			var data = {
+						clubName : clubName,
+						universityAt : universityAt
+					},
+					secret = hashesFactory.sha256(key + JSON.stringify(data));
 			return $http.post('/api/club/post/listNews', {
-				clubName : clubName,
-				universityAt : universityAt
+				userType : 'student',
+				secret : secret,
+				email : email,
+				data : data
 			});
 		}
 
-		News.prototype.postNews = function(clubName, universityAt, news, firstName, lastName){
+		News.prototype.postNews = function(clubName, universityAt, news, firstName, lastName, userType, email, key){
+			var data = {
+						clubName  : clubName,
+						universityAt : universityAt,
+						news : news,
+						firstName : firstName,
+						lastName : lastName
+					},
+					secret = hashesFactory.sha256(key + JSON.stringify(data));
 			return $http.post('/api/club/post/postNews', {
-				clubName  : clubName,
-				universityAt : universityAt,
-				news : news,
-				firstName : firstName,
-				lastName : lastName
+				userType : 'student',
+				secret : secret,
+				email : email,
+				data : data
 			});
 		}
 
-		News.prototype.deleteNews = function(postId){
-			return $http.post('/api/club/post/deleteNews', { postId : postId });
-		}
-
-		Comment.prototype.addComment = function(postId, comment, commenterFirstName, commenterLastName){
-			return $http.post('/api/club/post/comment/addComment',{
-				postId : postId,
-				comment : comment,
-				commenterFirstName : commenterFirstName,
-				commenterLastName : commenterLastName
+		News.prototype.deleteNews = function(postId, userType, email, key){
+			var data = { postId : postId },
+					secret = hashesFactory.sha256(key + JSON.stringify(data));
+			return $http.post('/api/club/post/deleteNews', { 
+				userType : 'student',
+				secret : secret,
+				email : email,
+				data : data
 			});
 		}
 
-		Comment.prototype.editComment = function(postId, commentId, comment){
-			return $http.post('/api/club/post/comment/editComment',{
-				postId : postId,
-				commentId : commentId,
-				comment : comment
+		Comment.prototype.addComment = function(postId, comment, commenterFirstName, commenterLastName, userType, email, key){
+			var data = {
+						postId : postId,
+						comment : comment,
+						commenterFirstName : commenterFirstName,
+						commenterLastName : commenterLastName
+					},
+					secret = hashesFactory.sha256(key + JSON.stringify(data));
+			return $http.post('/api/club/post/comment/addComment', {
+				userType : 'student',
+				secret : secret,
+				email : email,
+				data : data
 			});
 		}
 
-		Comment.prototype.removeComment = function(postId, commentId){
+		Comment.prototype.editComment = function(postId, commentId, comment, userType, email, key){
+			var data = {
+						postId : postId,
+						commentId : commentId,
+						comment : comment
+					},
+					secret = hashesFactory.sha256(key + JSON.stringify(data));
+			return $http.post('/api/club/post/comment/editComment', {
+				userType : 'student',
+				secret : secret,
+				email : email,
+				data : data
+			});
+		}
+
+		Comment.prototype.removeComment = function(postId, commentId, userType, email, key){
+			var data = {
+						postId : postId,
+						commentId : commentId
+					},
+					secret = hashesFactory.sha256(key + JSON.stringify(data));
 			return $http.post('/api/club/post/comment/removeComment', {
-				postId : postId,
-				commentId : commentId
+				userType : 'student',
+				secret : secret,
+				email : email,
+				data : data
 			});
 		}
 
