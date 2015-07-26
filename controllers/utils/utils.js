4d5b77d981579278de96f8e1cf2b01a4b6ba8787
slimfire <personalprojectyohannes@gmail.com>
2015-07-26 00:21:34 -0700

    [FEATURE] add crud, constants and modelFactory into the wrapper object

--- controllers/utils/utils.js
+++ controllers/utils/utils.js
@@ -1,5 +1,8 @@
 module.exports = {
 	request : require('./request.js'),
 	sha : require('./sha.js'),
-	signinType : require('./signinType.js')
+	signinType : require('./signinType.js'),
+	crud : require('./crud/crud.js'),
+	constants : require('./constants.js'),
+	model : require('./model.js')
 };
