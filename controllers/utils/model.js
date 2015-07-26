e6d80cdd72c245a2d2c184cabe1a8836c18e3e02
slimfire <personalprojectyohannes@gmail.com>
2015-07-25 20:53:13 -0700

    [REFACTOR] move model factory into utilities

---
+++ controllers/utils/model.js
@@ -0,0 +1,36 @@
+var models = require('../../models/models.js');
+
+var Model = function(){}
+
+Model.prototype.get = function(modelName, callback){
+	if(modelName == 'clubModel')
+	{
+		callback(models.clubModel);
+	}
+	else if(modelName == 'adminModel')
+	{
+		callback(models.adminModel);	
+	}
+	else if(modelName == 'studentModel')
+	{
+		callback(models.studentModel);	
+	}
+	else if(modelName == 'newClubRequestModel')
+	{
+		callback(models.newClubRequestModel);	
+	}
+	else if(modelName == 'clubRemovalRequestModel')
+	{
+		callback(models.clubRemovalRequestModel);	
+	}
+	else if(modelName == 'postModel')
+	{
+		callback(models.postModel);	
+	}	
+	else
+	{
+		callback("Error : The model name you are looking is not available. Check the spelling.");
+	}
+}
+
+module.exports = new Model();
