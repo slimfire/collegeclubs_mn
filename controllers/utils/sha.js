0f0225827ecead4787fe789cae47af37648461e9
slimfire <personalprojectyohannes@gmail.com>
2015-07-25 23:35:49 -0700

    [TASK] remove log

--- controllers/utils/sha.js
+++ controllers/utils/sha.js
@@ -4,8 +4,6 @@ var crypto = require('crypto'),
 var Sha = function(){}
 
 Sha.prototype.hash = function(string){
-	console.log('hashing : ', string, " of type : ", typeof(string))
-
 	if(typeof(string) == 'string')
 	{
 		var shaString = crypto.createHash('sha256');
